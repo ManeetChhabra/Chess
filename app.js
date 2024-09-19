e6f5ea5a84153e8a250617ea546f0c26a0f7b810
ManeetChhabra <130479331+ManeetChhabra@users.noreply.github.com>
2024-09-19 15:49:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ io.on("connection", function (uniquesocket) {
   });
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT||3000, () => {
   console.log("Server is running on port 3000");
 });
